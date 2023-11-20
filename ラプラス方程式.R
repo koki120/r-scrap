89a65d4f337978b4276941393cb2618e326cebb1
koki120 <koki20001206@outlook.jp>
2023-11-20 05:47:46 +0000

    add ラプラス方程式/問1

--- ラプラス方程式.R
+++ ラプラス方程式.R
@@ -1,10 +1,10 @@
 library(scatterplot3d)
 
-# ラプラス方程式の定義
-laplace_solver <- function(x, y) {
+# 問2のラプラス方程式の定義
+laplace_solver2 <- function(x, y) {
   result = 0
-  for (n in seq(1, 99, 2)) {
-    result = result + (4/n*pi)*sin(n*pi*x/50)*sinh(n*pi*y/50)/sinh(n*pi)
+  for (n in seq(1, 120, 2)) {
+    result = result + (4/(n*pi))*sin(n*pi*x/50)*sinh(n*pi*y/50)/sinh(n*pi)
   }
   return(result)
 }
@@ -14,6 +14,28 @@ x = y = seq(0, 50, length.out = 80)
 
 
 # 2変数関数の値を計算
-z = outer(x, y, laplace_solver)
+z = outer(x, y, laplace_solver2)
 
-persp(x, y, z, theta=300, phi=20, expand=0.5, ticktype="detailed",border="blue")
+persp(x, y, z, theta=60, phi=20, expand=0.5, ticktype="detailed",border="blue")
+
+
+
+# 問1のラプラス方程式の定義
+laplace_solver1 <- function(n) {
+  x = 30
+  y = 50
+  result = 0
+  for (m in seq(1, n, 2)) {
+    result = result + 4*sin(m*pi*x/50)*sinh(m*pi*y/50)/((m*pi)*sinh(m*pi))
+  }
+  return(result)
+}
+
+for (n in seq(1,100,2)) {
+  diff = 1- laplace_solver1(n)
+  if (abs(diff) < 0.01) {
+    print(sprintf("初めて条件を満たす項は %d です。", (n + 1) / 2))
+    break
+  }
+}
+
